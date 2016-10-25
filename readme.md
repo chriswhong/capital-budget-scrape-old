d8879296b26eeff053e981b3c9bc3a21d54cdad8
Chris Whong <chris@whong.org>
2016-10-25 11:37:13 -0400

    ship it

---
+++ readme.md
@@ -0,0 +1,27 @@
+#captial-budget-scrape
+
+A node.js scraper for the NYC capital budget
+
+##What does it do?
+
+It pulls a bare minimum of information from the [FY17 capital budget pdf](http://www1.nyc.gov/assets/omb/downloads/pdf/cb6-16.pdf).
+
+Specifically, it is grabbing data from the "geographic analysis" in the back of the pdf:
+
+`boro` - The geography, either one of the five boroughs or "citywide"
+`budgetline` - the project type plus a code, this is also the unique id
+`type` - the project's type, which is a one or two character code.  Find the codes on page 4 fo the budget pdf, and see what agencies they align with.  Some agencies have multiple codes
+`description` - the description of the project - this is truncated from the full descriptions that appear earlier in the pdf, but are good enough to understand what the project is about without being too verbose
+
+Funding Fields: `fy17cn`,`fy17cx`,`fy17f`,`fy17s`,`fy17p`,`fy17total`
+
+CN - City Non-exempt
+CX - City Exempt
+F - Federal
+S - State
+P - Private
+Total - Sum of all of these
+
+##Total 
+The sum of the `fy17total` column should be `14007015000` ($14B)
+You will find a similar number in the totals on page 8 of the PDF.  It is slightly different because the budget lines listed in the gepgraphic analysis are in thousands of dollars, while the budget lines in the earlier sections are accurate to one dollar.  
--- scrape.js
+++ scrape.js
@@ -1,6 +1,6 @@
-//Scrapes Published OMB Budget Documents
-//Include source pdf and output filename as arguments like:
-//node scrape input.pdf ./output.csv
+//scrape.js - scrapes the NYC FY2017 Capital Budget
+//Include source pdf and output filename as 1st and 2nd arguments like:
+//node scrape .pdf/input.pdf ./data/fy17capitalbudget.csv
 
 //dependencies
 var extract = require('pdf-text-extract'),
@@ -23,21 +23,13 @@ var config = {
   ]  
 }
 
-
-
+//create output file and write the header row
 var outputFile = fs.createWriteStream(config.outputPath)
-
-
-//write the headers to the output csv first
 outputFile.write(config.headers.join(',') + '\n')
 
-//extract gives an array of stings, each string contains the text of a full page
-//we are passing an empty options object AND a path to the pdftotext executable
-//so that we don't have to edit PATH in windows (which we don't have rights to do)
+//extract is calling pdftotext, returning an array of pages of text
 extract(config.inputPath, {}, function (err, pages) {
   console.log('Pulled ' + pages.length + ' pages of text from ' + config.inputPath)
-  //we only want 12 thru 399 
-  // for (var i=12; i<399; i++) {
 
   var budgetLines = []
 
@@ -46,6 +38,7 @@ extract(config.inputPath, {}, function (err, pages) {
     adoptedBudget: []
   }
 
+  //create array to match the geography lines of the geographic report
   var boroLines = [
     'C I T Y W I D E',
     'B R O N X',
@@ -68,16 +61,20 @@ extract(config.inputPath, {}, function (err, pages) {
   rowData = null,
   budgetLine
 
+  //pp422-542 are the geographic analysis, which includes the 2017 budget amounts we are interested in
   for (var i=422; i<542; i++) {
 
     var firstPageText = pages[i-1];
     var lines = firstPageText.split('\n')
     console.log(lines)
     lines.map(function(line) {
+
+      //check if the line contains text for one of the geographies, if so, set geography (boro)
       if(boroLines.indexOf(line) > -1) {
         boro=boroText[boroLines.indexOf(line)]
       }
 
+      //if this is a line separator, we should probably write the row to the output
       if(line.match('------------------------------------------------------------------------------------------------------------------------------------')) {
         console.log(rowData)
         if(rowData) writeRow(rowData)
@@ -95,39 +92,25 @@ extract(config.inputPath, {}, function (err, pages) {
         }
       }  
 
-
-
+      //if rowData isn't null, grab the rest of the goodies
       if(rowData) {
         //grab the description
         rowData.description += line.substring(11,54).trim() + ' '
 
-        //regex to find amounts
-
+        //regex to find 2017 amounts, and only the 2017 amounts
         var matchAmount = line.substring(69, 133).match(/\d{1,3}(,\d{3})*(\.\d+)?\s*\(([A-Z]*)\)/)
         
-
+        //clean up numbers and append the appropriate key in the output object.  1,000 (CN) should end up being fy17cn: 1000
         if(matchAmount) {
           var fy17 = parseValue('fy17', matchAmount[0].trim())
           rowData[fy17.key] = fy17.amount
         }
-       
       }
-
     })
   }
 })
 
-
-//grabs fy17, 18, 19, 20 from source array starting at position i
-function parseAmounts(source, index, data) {
-  var yearKeys = ['fy17', 'fy18', 'fy19', 'fy20', 'rc']
-  for(var i=0; i<5; i++) {
-    var value = parseValue(yearKeys[i], source[i+index])
-    data[value.key] = value.amount
-  }
-}
-
-
+//given a base key (fy17) and a parsed value from the pdf ('10,050 (CX)'), return an object with a more descriptive key and cleaned $ amount 
 function parseValue(key, value) {
   console.log(value)
   var match = value.match(/(.*)\((.*)\)/)
@@ -137,10 +120,9 @@ function parseValue(key, value) {
       amount: parseFloat(match[1].replace(/,/g, '') * 1000)
     }    
   }
-
-
 }
 
+//take the rowData object and write it to a new line in the csv
 function writeRow(rowData) {
   
   var dollarFields = ['fy17CN', 'fy17CX', 'fy17F', 'fy17S', 'fy17P']
@@ -166,6 +148,8 @@ function writeRow(rowData) {
   
   outputFile.write(rowDataArray.join(',') + '\n')
 }
+
+//IMPORTANT!  MANUAL CHANGE TO OUTPUT FILE REQUIRED UNTIL WE CAN FIX THIS:
 
 //Budget Line E-2364 is not formatted the same as the others!!!
 //The regex for finding comma-delimited numbers only grabs its last 3 digits,  
